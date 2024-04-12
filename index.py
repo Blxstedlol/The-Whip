7b0e6f564da2fbfa804155d871edb7ecb197b4fb
Blxsted <lostblxsted@gmail.com>
2024-04-12 19:49:02 +0100

    Added a bunch new algorithms

--- index.py
+++ index.py
@@ -10,9 +10,10 @@ megenta = Fore.MAGENTA
 yellow = Fore.LIGHTYELLOW_EX
 reset = Fore.RESET
 white = Fore.WHITE
+lightcyan = Fore.LIGHTCYAN_EX
 
 print(f"""
-{white}
+{lightcyan}
  ______ __                  __      __  __                           
 /\__  _/\ \                /\ \  __/\ \/\ \      __                  
 \/_/\ \\ \ \___      __    \ \ \/\ \ \ \ \ \___ /\_\  _____          
@@ -26,8 +27,10 @@ print(f"""
 {blue} 1: Setup
 {green} 2: Run
 {red} 3: Info  
+{Fore.LIGHTBLACK_EX}            OTHER FEATURES:
 {white} 4: Password generator
 {yellow} 5: Salter
+{lightcyan} 6: Hasher
 {reset}
       made by blxsted
       """)
@@ -39,43 +42,95 @@ if mode == 1:
         settings.write(file_path)
 
 if mode == 2:
-    
+
     hash_to_crack = input(f'{blue} Hash to crack: ')
-    with open('settings.txt', 'r') as settings:
+    try:
+    
+    
+        with open('settings.txt', 'r') as settings:
+            file_path = settings.read()
+        alg_choice = input(f"""
+            {yellow}    1: SHA256
+                {megenta} 2: MD5  
+                {yellow} 3: SHA1
+                {megenta} 4: SHA224
+                {yellow} 5: SHA512
+                {megenta} 6: SHA384 
+                {reset}         
+                        """)
         
-        file_path = settings.read()
-    alg_choice = input(f"""
-          {yellow}    1: SHA256
-             {megenta} 2: MD5   
-             {reset}         
-                       """)
-    
-    
-    def sha256_hash(line):
-        line = line.strip()  
-        if line:  
-            hashed = hashlib.sha256(line.encode()).hexdigest()
-            print(green, hashed, ':', line)
-            if hashed == hash_to_crack:
-                print(f'{blue} PASSWORD FOUND: {line}')
-                exit('Reason for exit: found password')
+        
+        def sha256_hash(line):
+            line = line.strip()  
+            if line:  
+                hashed = hashlib.sha256(line.encode()).hexdigest()
+                print(green, hashed, ':', line)
+                if hashed == hash_to_crack:
+                    print(f'{blue} PASSWORD FOUND: {line}')
+                    exit('Reason for exit: found password')
 
-    def MD5_hash(line):
-        line = line.strip()  
-        if line:  
-            hashed = hashlib.md5(line.encode()).hexdigest()
-            print(green, hashed, ':', line)
-            if hashed == hash_to_crack:
-                print(f'{blue} PASSWORD FOUND: {line}')
-                exit('Reason for exit: found password')
+        def sha1_hash(line):
+            line = line.strip()  
+            if line:  
+                hashed = hashlib.sha1(line.encode()).hexdigest()
+                print(green, hashed, ':', line)
+                if hashed == hash_to_crack:
+                    print(f'{blue} PASSWORD FOUND: {line}')
+                    exit('Reason for exit: found password')
 
-    with open(file_path, 'r') as file:
-        for line in file:
-            if alg_choice == '1':
-                sha256_hash(line)
-            if alg_choice == '2':
-                MD5_hash(line)
+        def sha384_hash(line):
+            line = line.strip()  
+            if line:  
+                hashed = hashlib.sha384(line.encode()).hexdigest()
+                print(green, hashed, ':', line)
+                if hashed == hash_to_crack:
+                    print(f'{blue} PASSWORD FOUND: {line}')
+                    exit('Reason for exit: found password')
 
+        def sha512_hash(line):
+            line = line.strip()  
+            if line:  
+                hashed = hashlib.sha512(line.encode()).hexdigest()
+                print(green, hashed, ':', line)
+                if hashed == hash_to_crack:
+                    print(f'{blue} PASSWORD FOUND: {line}')
+                    exit('Reason for exit: found password')
+
+        def sha224_hash(line):
+            line = line.strip()  
+            if line:  
+                hashed = hashlib.sha224(line.encode()).hexdigest()
+                print(green, hashed, ':', line)
+                if hashed == hash_to_crack:
+                    print(f'{blue} PASSWORD FOUND: {line}')
+                    exit('Reason for exit: found password')
+
+        def MD5_hash(line):
+            line = line.strip()  
+            if line:  
+                hashed = hashlib.md5(line.encode()).hexdigest()
+                print(green, hashed, ':', line)
+                if hashed == hash_to_crack:
+                    print(f'{blue} PASSWORD FOUND: {line}')
+                    exit('Reason for exit: found password')
+
+        with open(file_path, 'r') as file:
+            for line in file:
+                if alg_choice == '1':
+                    sha256_hash(line)
+                if alg_choice == '2':
+                    MD5_hash(line)
+                if alg_choice == '3':
+                    sha1_hash(line)
+                if alg_choice == '4':
+                    sha224_hash(line)
+                if alg_choice == '5':
+                    sha512_hash(line)
+                if alg_choice == '6':
+                    sha384_hash(line)
+                
+    except FileNotFoundError as e:
+        print(f'{yellow}settings.txt not found. Please use setup or input 1 before running.', reset)
 if mode == 3:
     print('Made by blxsted, use it to hack people idfc, just do not get caught')
 
